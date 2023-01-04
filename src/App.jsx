d19a5aaa54b36b8a78b937a366948b6098d1b291
Olumide-Z <ilesanmiolumide10@gmail.com>
2023-01-04 13:56:46 +0100

    hover effect

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="image-1">
+          <img src="/images/1.webp" alt="photo1" />
+        </div>
+        <div className="image-2">
+          <img src="/images/2.webp" alt="photo2" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
